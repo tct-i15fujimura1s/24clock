0b03e63550e57051d65778e01bbe4a3f88fca864
tct-i15fujimura1s <44060475+tct-i15fujimura1s@users.noreply.github.com>
2019-01-15 21:31:39 +0900

    Update mode60.c

--- libs/mode60.c
+++ libs/mode60.c
@@ -9,48 +9,22 @@ enum TURN current_turn = TURN_RED;
 
 static unsigned int matrix[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
+#define O_IS_RED(x, y) (matrix[y] & 1 << x)
+#define O_IS_GREEN(x, y) (matrix[y] & 0x100 << x)
+
+inline void O_SET_RED(x, y){
+	matrix[y] = matrix[y] & ~(0x100 << x) | (0x1 << x);
+}
+
+inline void O_SET_GREEN(x, y){
+	matrix[y] = matrix[y] & ~(0x1 << x) | (0x100 << x);
+}
+
 int pointer_x = 0, pointer_y = 0;
 int show_pointer = FALSE;
 
-void draw_board() {
-	static int col = 0;
-	/*16bit (1列分)をシリアル転送*/
-	col = col + 1 & 7;
-	unsigned int p;
-	for(p = matrix[col], i = 0; i < 16; i++, p <<= 1){
-		if((p&0x8000)==0){
-			DISABLE_MATRIX_SIN();
-		}else{
-			ENABLE_MATRIX_SIN();
-		}
-	}
-	SET_H_MATRIX_SCLK();
-	SET_L_MATRIX_SCLK();
-	ENABLE_MATRIX_BLANK();
-	DISABLE_MATRIX_LATCH();
-	ENABLE_MATRIX_LATCH();
-	SELECT_MATRIX_COLUMN(col);
-	DISABLE_MATRIX_BLANK();
-}
-
-void lcd_refresh(){
-	lcd_clear();
-	lcd_putstr(0,0,"MODE7:OTHELLO");
-	if(current_turn == TURN_RED){
-		lcd_putstr(0,1,"  put  pass");
-		if(show_pointer){
-			lcd_putstr(0, 1, "\xc6\xb1put      UDLRC");
-		}else{
-			lcd_putstr(13, 1, "LRC");
-			lcd_putstr(pointer_x == 0 ? 4 : 9, 1, "\xc6\xb1"); //「ﾆｱ」
-		}
-		//TODO: make the full-color led red
-		ENABLE_LED_RED();
-	}else{
-		//TODO: make the full-color led green
-		ENABLE_LED_GREEN();
-	}
-}
+void draw_board();
+void lcd_refresh();
 
 void do_mode60(UI_DATA *ui_data){
 	if(ui_data->prev_mode != ui_data->mode) {
@@ -58,11 +32,11 @@ void do_mode60(UI_DATA *ui_data){
 		//オセロ盤の初期化
 		//((unsigned short *) red)[0] = 0x0000; 
 		red[0] = red[1] = red[2] = 0x00;
-		red[5] = red[6] = red[7] = 0x00;
 		green[0] = green[1] = green[2] = 0x00;
-		green[5] = green[6] = green[7] = 0x00;
 		red[3] = green[4] = 0x10;
 		green[3] = red[4] = 0x08;
+		red[5] = red[6] = red[7] = 0x00;
+		green[5] = green[6] = green[7] = 0x00;
 
 		each_turn_begins();
 	}
@@ -103,5 +77,45 @@ void do_mode60(UI_DATA *ui_data){
 
 	if(sec_flag){
 		sec_flag = FALSE;
+	}
+}
+
+void draw_board() {
+	static int col = 0;
+	/*16bit (1列分)をシリアル転送*/
+	col = col + 1 & 7;
+	unsigned int p;
+	for(p = matrix[col], i = 0; i < 16; i++, p <<= 1){
+		if((p&0x8000)==0){
+			DISABLE_MATRIX_SIN();
+		}else{
+			ENABLE_MATRIX_SIN();
+		}
+	}
+	SET_H_MATRIX_SCLK();
+	SET_L_MATRIX_SCLK();
+	ENABLE_MATRIX_BLANK();
+	DISABLE_MATRIX_LATCH();
+	ENABLE_MATRIX_LATCH();
+	SELECT_MATRIX_COLUMN(col);
+	DISABLE_MATRIX_BLANK();
+}
+
+void lcd_refresh(){
+	lcd_clear();
+	lcd_putstr(0,0,"MODE7:OTHELLO");
+	if(current_turn == TURN_RED){
+		lcd_putstr(0,1,"  put  pass");
+		if(show_pointer){
+			lcd_putstr(0, 1, "\xc6\xb1put      UDLRC");
+		}else{
+			lcd_putstr(13, 1, "LRC");
+			lcd_putstr(pointer_x == 0 ? 4 : 9, 1, "\xc6\xb1"); //「ﾆｱ」
+		}
+		//TODO: make the full-color led red
+		ENABLE_LED_RED();
+	}else{
+		//TODO: make the full-color led green
+		ENABLE_LED_GREEN();
 	}
 }
