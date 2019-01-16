84683781617e53994adc121f25dfaa6182315d0c
tct-i15fujimura1s <44060475+tct-i15fujimura1s@users.noreply.github.com>
2019-01-16 12:22:58 +0900

    Update mode60.c

--- libs/mode60.c
+++ libs/mode60.c
@@ -6,7 +6,7 @@ enum TURN {
 };
 
 typedef struct {
-	char reserved:1;
+	char computes_openness:1;
 	char is_simulation:1;
 	char y:3;
 	char x:3;
@@ -16,15 +16,15 @@ static enum TURN current_turn = TURN_RED;
 
 static unsigned int matrix[8] = {0, 0, 0, 0, 0, 0, 0, 0};
 
-#define IS_RED(p) (matrix[(p.y)] & 1 << (p.x))
-#define IS_GREEN(p) (matrix[(p.y)] & 0x100 << (p.x))
+#define IS_RED(p) (matrix[(p.y)] & 0x100 << (p.x))
+#define IS_GREEN(p) (matrix[(p.y)] & 0x1 << (p.x))
 
 static inline void SET_RED(POS p){
-	matrix[p.y] = matrix[p.y] & ~(0x100 << p.x) | (0x1 << p.x);
+	matrix[p.y] = matrix[p.y] & ~(0x1 << p.x) | (0x100 << p.x);
 }
 
 static inline void SET_GREEN(POS p){
-	matrix[p.y] = matrix[p.y] & ~(0x1 << p.x) | (0x100 << p.x);
+	matrix[p.y] = matrix[p.y] & ~(0x100 << p.x) | (0x1 << p.x);
 }
 
 static POS pointer;
@@ -34,23 +34,20 @@ static void draw_board();
 static void lcd_refresh();
 static int put(enum TURN turn, POS p);
 static int flip(enum TURN turn, POS p, POS d);
-static int set(enum TURN turn, POS p);
+static void set(enum TURN turn, POS p);
 
-static int com_cycle = 0;
+static int cycle = 0;
 static void com_routine();
 
 void do_mode60(UI_DATA *ui_data){
 	if(ui_data->prev_mode != ui_data->mode) {
 		//マトリックスLEDの準備
 		//オセロ盤の初期化
-		//((unsigned short *) red)[0] = 0x0000; 
-		red[0] = red[1] = red[2] = 0x00;
-		green[0] = green[1] = green[2] = 0x00;
-		red[3] = green[4] = 0x10;
-		green[3] = red[4] = 0x08;
-		red[5] = red[6] = red[7] = 0x00;
-		green[5] = green[6] = green[7] = 0x00;
-
+		//((unsigned short *) red)[0] = 0x0000;
+		matrix[0] = matrix[1] = matrix[2] = 0x0000;
+		matrix[3] = 0x1008;
+		matrix[4] = 0x0810;
+		matrix[5] = matrix[6] = matrix[7] = 0x0000;
 		each_turn_begins();
 	}
 
@@ -154,23 +151,69 @@ static int put(enum TURN turn, POS p){
 	return flipped;
 }
 
+#define IS_WITHIN(x, a, b) ((x) >= (a) && (x) <= (b))
+#define IS_WITHIN_RECT8(p) (IS_WITHIN(p.x, 0, 7) && IS_WITHIN(p.y, 0, 7))
+#define P_ADD(p, d) (p).x += (d).x, (p).y -= (d).y
+#define P_SUB(p, d) (p).x -= (d).x, (p).y -= (d).y
 static int flip(enum TURN turn, POS p, POS d){
-	//TODO
+	int flipped = 0, i;
+	POS q, r, s, t;
+	if(turn == TURN_RED){
+		for(q = p, P_ADD(q, d); IS_WITHIN_RECT8(q); flipped++, P_ADD(q, d)){
+			if(IS_RED(q)) break;
+			if(!IS_GREEN(q)) return 0;
+		}
+		if(!p.is_simulation) for(q = p, P_ADD(q, d), i = 0; i < flipped; i++, P_ADD(q, d)) SET_RED(q);
+		//REDはプレイヤーなのでcomputes_opennessはいらない
+	}else{
+		for(q = p, P_ADD(q, d); IS_WITHIN_RECT8(q); flipped++, P_ADD(q, d)){
+			if(IS_GREEN(q)) break;
+			if(!IS_RED(q)) return 0;
+		}
+		if(!p.is_simulation) for(q = p, P_ADD(q, d), i = 0; i < flipped; i++, P_ADD(q, d)) SET_GREEN(q);
+		if(p.computes_openness){
+			q = p;
+			P_ADD(q, d);
+			s = q;
+			P_ADD(s, d);
+			t = q;
+			t.x = q.x + 1;
+			if(IS_WITHIN_RECT8(t) && !IS_RED(t) && !IS_GREEN(t)) flipped += 0x100;
+			t.x = q.x - 1;
+			if(IS_WITHIN_RECT8(t) && !IS_RED(t) && !IS_GREEN(t)) flipped += 0x100;
+			
+		}
+	}
+	return flipped;
 }
 
-static int set(enum TURN turn, POS p){
-	//TODO
+static void set(enum TURN turn, POS p){
+	if(turn == TURN_RED) SET_RED(p);
+	else SET_GREEN(p);
 }
 
+struct CANDIDATE {
+	unsigned char flips: 4;
+	unsigned char openness: 4;
+};
 static void com_routine(){
-	static int candidate[]; //
-	if(com_cycle >= 8){
-		com_cycle = 0;
+	static struct CANDIDATE candidates[64];
+	static int n_candidates;
+	POS p;
+	if(cycle >= 9){
+		cycle = 0;
 		current_turn = TURN_RED;
+		n_candidates = 0;
 		return;
+	}
+	if(cycle < 8){
+		p.y = cycle;
+		for(p.x = 0; p.x < 8; p.x++) {
+			
+		}
 	}
 	//評価値=-開放度*10+ひっくり返る石の数
 	//TODO
-	//8サイクルかけてゆっくり処理
-	com_cycle++;
+	//nサイクルかけてゆっくり処理
+	cycle++;
 }
