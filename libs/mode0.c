e4b1ab6033d126dbb1e682e85ecfd6184af4d425
tct-i15fujimura1s <44060475+tct-i15fujimura1s@users.noreply.github.com>
2019-01-17 16:15:52 +0900

    Update mode0.c

--- libs/mode0.c
+++ libs/mode0.c
@@ -1,6 +1,5 @@
 #include "libs.h"
 #include "main.h"
-#include "mode0.h"
 
 char mode0_title[] = HK_ME HK_NI HK_YU_SMALL HK_ONBIKI;
 
@@ -12,7 +11,7 @@ static int mode_add(int mm, int n){
 }
 
 void do_mode0(UI_DATA* ud){
-  static int next_mode_data=MODE_10;
+  static int next_mode_data=MODE_10, next_mode_data2 = MODE_20;
   int i;
 
   if(ud->prev_mode!=ud->mode){  /* 他のモードからモード0に遷移した時に実行 */
@@ -25,21 +24,25 @@ void do_mode0(UI_DATA* ud){
 
   if(tma_flag==TRUE){
     lcd_clear();
-    lcd_putstr(0,0,"-> MODE"); /*モード0の初期表示*/
+    lcd_putstr(0,0,"-> MODE");
+    lcd_putstr(8,0,mode_titles[next_mode_data/10]);
+    lcd_putstr(8,1,mode_titles[next_mode_data2/10]);
     lcd_putstr(0,1,"   MODE");
     lcd_putudec(7,0,1,next_mode_data/10);
-    lcd_putudec(7,1,1,mode_add(next_mode_data,10)/10);
+    lcd_putudec(7,1,1,next_mode_data2/10);
     tma_flag=FALSE;
   }
 
   switch(ud->sw){  /*モード内でのキー入力別操作*/
 
   case KEY_SHORT_U: /* 上短押し */
+    next_mode_data2 = next_mode_data;
     next_mode_data = mode_add(next_mode_data,-10);
     break;
 
   case KEY_SHORT_D: /* 下短押し */
     next_mode_data = mode_add(next_mode_data,+10);
+    next_mode_data2 = mode_add(next_mode_data,+10);
     break;
     
   case KEY_LONG_C: /* 中央キーの長押し */
