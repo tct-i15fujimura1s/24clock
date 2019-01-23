5bdfe8c41e3b1f299f9cc1a6597061cb58865dca
i15kawamoto <i15kawamoto@iebsd35.ie.tokuyama.ac.jp>
2019-01-23 14:11:44 +0900

    updata

--- libs/mode20.c
+++ libs/mode20.c
@@ -3,59 +3,104 @@
 #include "mode20.h"
 
 
-static char time_data_arr[9]={03,04,05,06,07,08,09,10,\0};
+static char time_data_arr[7]={'3','4','5','6','7','8','9'};
+volatile int time_arr[7]={180,240,300,360,420,480,540};
+static char temp[2] = {0,0};
 static int i = 0;
 static int j = 1;
+volatile int waittime = 0;
+static int tempo=120;
 
 void do_mode20(UI_DATA* ud){
 
+
 lcd_clear();
 lcd_putstr(0,0,"->");
-lcd_putstr(2,0,time_data_arr[i]);
-lcd_putstr(4,0,"ﾌﾝﾀｲﾏｰ");
-lcd_putstr(2,1,time_data_arr[j]);
-lcd_putstr(4,1,"ﾌﾝﾀｲﾏｰ");
+   temp[0] = time_data_arr[i];
+lcd_putstr(2,0,temp);
+lcd_putstr(3,0,"mintimer");
+   temp[0] = time_data_arr[j];
+lcd_putstr(2,1,temp);
+lcd_putstr(3,1,"mintimer");
 
 
-switch(ud->sw){
+ switch(ud->sw){
 
-case KEY_SHORT_U:
-     i=i+1;
-     j=j+1;
-     if(i>8){
-	i=0;
-     }
-     if(j>8){
-	j=0;
-     }
-     break;
+ case KEY_SHORT_U:
+   i=i+1;
+   j=j+1;
+   if(i>6){
+     i=0;
+   }
+   if(j>6){
+     j=0;
+   }
+   break;
+   
+ case KEY_SHORT_D:
+   i=i-1;
+   j=j-1;
+   if(i<0){
+     i=6;
+   }
+   if(j<0){
+     j=6;
+   }
+   break;
+   
+ case KEY_LONG_R:
+   waittime = time_arr[i];
+   ud->mode=MODE_21;/*次は，モード変更*/
+   break;
+   
+ case KEY_LONG_C: /* 中央キーの長押し */
+   lcd_cursor(FALSE);
+   lcd_blink(FALSE);
+   ud->mode=MODE_0; /* メニューに移動 */
+   break;
+   
+   
+ default:
+   break;
+   
+   
+}
+}
+	
+void do_mode21(UI_DATA* ud){
+  tempo_compare = 3735 / tempo; /* 1000/(tempo * 16 / 60) を展開 */
+  temp[0]=time_data_arr[i];
+  lcd_clear();
+  lcd_putstr(0,0,temp);
+  lcd_putstr(1,0,"mintimer");
+  lcd_putudec(0,1,3,waittime);
+  lcd_putstr(3,1,"secwait...");
 
-case KEY_SHORT_D:
-     i=i-1;
-     j=j-1;
-     if(i<0){
-	i=8;
-     }
-     if(j<0){
-	j=8;
-     }
-     break;
 
-case KEY_LONG_R:
-     ud->mode=MODE_20+i;/*次は，モード変更*/
-     break;
-
-case KEY_LONG_C: /* 中央キーの長押し */
+  if(sec_flag==TRUE) {
+    waittime -= 1;
+    if(waittime==0){
+      snd_play("C C C ");
+      lcd_cursor(FALSE);
+      lcd_blink(FALSE);
+      ud->mode=MODE_0; /* メニューに移動 */
+    }
+    sec_flag=FALSE;
+  }
+  switch(ud->sw){
+  case KEY_LONG_C: /* 中央キーの長押し */
     lcd_cursor(FALSE);
     lcd_blink(FALSE);
     ud->mode=MODE_0; /* メニューに移動 */
     break;
+    
+  case KEY_LONG_L:
+    ud->mode=MODE_20;/*次は，モード変更*/
+    break;
+    
+  default:
+    break;
 
-
-default:
- break;
-
-
+  }
+  
 }
-}
-	
