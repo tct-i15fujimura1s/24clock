19e18ccb15e48f58d1aa01006e35d25247483e60
i15yokota <i15yokota@iebsd32.ie.tokuyama.ac.jp>
2019-01-23 15:06:11 +0900

    完成した

--- libs/mode40.c
+++ libs/mode40.c
@@ -3,7 +3,6 @@
 #include "mode40.h"
 
 static unsigned int pattern[8];
-static unsigned char matrix_led_pattern1[8][8];
 static unsigned int x=0,y=0;
 
 void do_mode40(UI_DATA* ud){
@@ -11,21 +10,9 @@ void do_mode40(UI_DATA* ud){
    if(ud->prev_mode!=ud->mode){  /* 他のモード遷移した時に実行 */
     /*必要なら，何らかのモードの初期化処理*/
     lcd_clear();
-
-    //赤色に光る
-    for(i=0;i<8;i++){
-	matrix_led_pattern1[0][i] = 0x0001;
-	matrix_led_pattern1[1][i] = 0x0002;
-	matrix_led_pattern1[2][i] = 0x0004;
-	matrix_led_pattern1[3][i] = 0x0008;
-	matrix_led_pattern1[4][i] = 0x0010;
-	matrix_led_pattern1[5][i] = 0x0020;
-	matrix_led_pattern1[6][i] = 0x0040;
-	matrix_led_pattern1[7][i] = 0x0080;
-    }
-    //どこを光らせるか 
+    lcd_putstr(0,0,"MODE4:OEKAKI");
    }
-
+   
    if(tma_flag == TRUE){
      for(i = 0; i < 8; i++){//コピー
        matrix_led_pattern[i] = pattern[i];
@@ -36,28 +23,31 @@ void do_mode40(UI_DATA* ud){
     
     /*モード4は，真中ボタンが押されたら，MODE0に戻る*/
   switch(ud->sw){    /*モード内でのキー入力別操作*/
-  case KEY_LONG_C:   /* 中央キーの長押し */
+  case KEY_LONG_C:/* 中央キーの長押し */
+    for(i = 0; i < 8; i++)
+      matrix_led_pattern[i] = 0x0000;
     ud->mode=MODE_0; /* 次は，モード0に戻る */
     break;
   case KEY_SHORT_L:
-    x=x+7;
+    x=(x+7)%8;
     break;
   case KEY_SHORT_R:
-    x++;
-    break;
-  case KEY_SHORT_U:
-    y++;
+   x=(x+1)%8;
     break;
   case KEY_SHORT_D:
-    y=y+7;
+    y=(y+1)%8;
+    break;
+  case KEY_SHORT_U:
+    y=(y+7)%8;
     break;
   case KEY_SHORT_C:
     col = pattern[x];
+    lcd_putdec(0,1,5,col);
     if(col & (0x0001<<y)){//もし赤なら
       col &= ~(0x0001<<y);//赤を消す
       col |= (0x0100<<y);//緑をつける
     }
-    else if(col &= 0x0100<<y)//緑なら
+    else if(col & 0x0100<<y)//緑なら
       col &= ~(0x0100 <<y);//緑を消す
     else col |= (0x0001<<y);//ついてなかったら赤をつける
     pattern[x] = col;
