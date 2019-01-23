d64d8ddceefb822e5288322f3a5b98cccc72acf3
Takajo-Soft05 <44186790+Takajo-Soft05@users.noreply.github.com>
2019-01-23 16:20:13 +0900

    Update mode30.c

--- libs/mode30.c
+++ libs/mode30.c
@@ -3,95 +3,148 @@
 
 char mode30_title[] = HK_YA HK_SI HK_DAKUTEN HK_RU HK_SI;
 
-static unsigned int l=0,r=0,u=0,d=0;
-static const unsigned int pattern[8] = {0x0018, 0x003c, 0x007e, 0x00db, 0x0099, 0x0018, 0x0018, 0x0018};
-
+static unsigned int matrix_led_pattern[8];
+static unsigned int l=0,u=0,c=0,i;
+static unsigned int matrix_pattern[10][8]={
+  {0x1800,0x3c00,0x7e00,0xdb00,0x9900,0x1800,0x1800,0x1800},
+  {0x0018,0x003c,0x007e,0x00db,0x0099,0x0018,0x0018,0x0018},
+  {0x1800,0x1800,0x1800,0x9900,0xdb00,0x7e00,0x3c00,0x1800},
+  {0x0018,0x0018,0x0018,0x0099,0x00db,0x007e,0x003c,0x0018},
+  {0x1800,0x0c00,0x0600,0xff00,0xff00,0x0600,0x0c00,0x1800},
+  {0x0018,0x000c,0x0006,0x00ff,0x00ff,0x0006,0x000c,0x0018},
+  {0x1800,0x3000,0x6000,0xff00,0xff00,0x6000,0x3000,0x1800},
+  {0x0018,0x0030,0x0060,0x00ff,0x00ff,0x0060,0x0030,0x0018},
+  {0xffff,0xffff,0xffff,0xffff,0xffff,0xffff,0xffff,0xffff},
+  {0xff00,0xff00,0xff00,0xff00,0xff00,0xff00,0xff00,0xff00}};
 void do_mode30(UI_DATA* ud){
-  int i;
   switch(ud->sw){  /*モード内でのキー入力別操作*/
   case KEY_LONG_C:  /* 中央キーの長押し */
     ud->mode=MODE_0; /* 次は，モード0に戻る */
     break;
-    
   case KEY_SHORT_L:
-    if(l==0){
-      for(i = 0; i < 8; i++){
-	matrix_led_pattern[i] = pattern[i];
+    if(c==0){
+      //コース1
+      if(l<=1 && u==2){
+	l++;
+	for(i=0;i<8;i++)
+	  matrix_led_pattern[i]=matrix_pattern[0][i];
+      }else{
+	for(i=0;i<8;i++)
+	  matrix_led_pattern[i]=matrix_pattern[1][i];
       }
     }else{
-      for(i = 0; i < 8; i++){
-	matrix_led_pattern[i] = ((pattern[i] & 0xff) << 8) | (pattern[i] >> 8);
+      //コース2
+      if((l==1 && (u==0 || u==3)) || (l==2 && (u==1 || u==3))){
+	//進めた時
+	l--;
+	//スタート位置判定
+	if(u==0 && l==0){
+	  for(i=0;i<8;i++)
+	    matrix_led_pattern[i]=matrix_pattern[8][i];
+	}else{
+	  for(i=0;i<8;i++)
+	    matrix_led_pattern[i]=matrix_pattern[0][i];
+	}
+      }else{
+	for(i=0;i<8;i++)
+	  matrix_led_pattern[i]=matrix_pattern[1][i];
       }
     }
-    l=!l;
     break;
-    
   case KEY_SHORT_R:
-    if(r==0){
-      for(i = 0; i < 8; i++){
-	matrix_led_pattern[7 - i] = pattern[i];
+    if(c==0){
+      if(0<l && u==2){
+	l--;
+	for(i=0;i<8;i++)
+	  matrix_led_pattern[i]=matrix_pattern[2][i];
+      }else{
+	for(i=0;i<8;i++)
+	  matrix_led_pattern[i]=matrix_pattern[3][i];
       }
     }else{
-      for(i = 0; i < 8; i++){
-	matrix_led_pattern[7 - i] = ((pattern[i] & 0xff) << 8) | (pattern[i] >> 8);
+      if((l==0 && (u==0 || u==3)) || (l==1 && (u==1 || u==3))){	  
+	for(i=0;i<8;i++)
+	  matrix_led_pattern[i]=matrix_pattern[2][i];
+	l++;
+      }else{
+	for(i=0;i<8;i++)
+	  matrix_led_pattern[i]=matrix_pattern[3][i];
       }
     }
-    r=!r;
     break;
-    
   case KEY_SHORT_U:
-    if(u==0){
-      matrix_led_pattern[0]=0x0018;
-      matrix_led_pattern[1]=0x000c;
-      matrix_led_pattern[2]=0x0006;
-      matrix_led_pattern[3]=0x00ff;
-      matrix_led_pattern[4]=0x00ff;
-      matrix_led_pattern[5]=0x0006;
-      matrix_led_pattern[6]=0x000c;
-      matrix_led_pattern[7]=0x0018;
-      u=1;
+    if(c==0){
+      if(u<=1){
+	u++;
+	for(i=0;i<8;i++)
+	  matrix_led_pattern[i]=matrix_pattern[4][i];
+      }else{
+	for(i=0;i<8;i++)
+	  matrix_led_pattern[i]=matrix_pattern[5][i];
+      }
     }else{
-      matrix_led_pattern[0]=0x1800;
-      matrix_led_pattern[1]=0x0c00;
-      matrix_led_pattern[2]=0x0600;
-      matrix_led_pattern[3]=0xff00;
-      matrix_led_pattern[4]=0xff00;
-      matrix_led_pattern[5]=0x0600;
-      matrix_led_pattern[6]=0x0c00;
-      matrix_led_pattern[7]=0x1800;
-      u=0;
+      if((l==1 && u==1) || (l==2 && (u==2 || u==3))){
+	for(i=0;i<8;i++)
+	  matrix_led_pattern[i]=matrix_pattern[4][i];
+	u--;
+      }else{
+	for(i=0;i<8;i++)
+	  matrix_led_pattern[i]=matrix_pattern[5][i];
+      }
     }
     break;
-    
   case KEY_SHORT_D:
-    if(d==0){
-      matrix_led_pattern[0]=0x0018;
-      matrix_led_pattern[1]=0x0030;
-      matrix_led_pattern[2]=0x0060;
-      matrix_led_pattern[3]=0x00ff;
-      matrix_led_pattern[4]=0x00ff;
-      matrix_led_pattern[5]=0x0060;
-      matrix_led_pattern[6]=0x0030;
-      matrix_led_pattern[7]=0x0018;
-      d=1;
+    if(c==0){
+      if(0<u && l!=1){
+	u--;
+	if(u==0){
+	  if(l==2){
+	    for(i=0;i<8;i++)
+	      matrix_led_pattern[i]=matrix_pattern[9][i];
+	  }
+	  if(l==0){
+	    for(i=0;i<8;i++)
+	      matrix_led_pattern[i]=matrix_pattern[8][i];
+	  }
+	}else{
+	  for(i=0;i<8;i++)
+	    matrix_led_pattern[i]=matrix_pattern[6][i];
+	}
+      }else{
+	for(i=0;i<8;i++)
+	  matrix_led_pattern[i]=matrix_pattern[7][i];
+      }
     }else{
-      matrix_led_pattern[0]=0x1800;
-      matrix_led_pattern[1]=0x3000;
-      matrix_led_pattern[2]=0x6000;
-      matrix_led_pattern[3]=0xff00;
-      matrix_led_pattern[4]=0xff00;
-      matrix_led_pattern[5]=0x6000;
-      matrix_led_pattern[6]=0x3000;
-      matrix_led_pattern[7]=0x1800;
-      d=0;
+      if((l==2 && (u==1 || u==2)) || (l==1 && u==0) || (l==0 && u==3)){
+	u++;
+	if(u==4){
+	  for(i=0;i<8;i++)
+	    matrix_led_pattern[i]=matrix_pattern[9][i];
+	}else{
+	  for(i=0;i<8;i++)
+	    matrix_led_pattern[i]=matrix_pattern[6][i];
+	}
+      }else{
+	for(i=0;i<8;i++)
+	  matrix_led_pattern[i]=matrix_pattern[7][i];
+      }
+      break;
+      case KEY_SHORT_C:
+	for(i=0;i<8;i++)
+	  matrix_led_pattern[i]=matrix_pattern[8][i];
+	if(c==0)
+	  c=1;
+	else c=0;
+	l=0;
+	u=0;
+	break;
     }
-    break;
   }
   if(ud->prev_mode!=ud->mode || sec_flag==TRUE){ 
     /* 他のモード遷移した時に実行 もしくは，1秒ごとに表示*/
     /*必要なら，何らかのモードの初期化処理*/
     lcd_clear();  //0123456789ABCDEF
-    lcd_putstr(3,0,"\xCE\xDE\xC0\xDD\xA6\xB5\xBC\xC3\xC8"); /*モード2の初期表示: ボタンヲオシテネ*/
+    lcd_putstr(4,0,"ﾐﾁｦﾐﾂｹﾃﾈ"); /*モード2の初期表示*/
     //show_sec();
     sec_flag=FALSE;
   }
@@ -100,6 +153,6 @@ void do_mode30(UI_DATA* ud){
   switch(ud->sw){    /*モード内でのキー入力別操作*/
   case KEY_LONG_C:   /* 中央キーの長押し */
     ud->mode=MODE_0; /* 次は，モード0に戻る */
-   break;
+    break;
   }
 }
